c060bcc296683d833ddbd2dbb08fd7bdb2216bf2
Sam-690 <56923470+Sam-690@users.noreply.github.com>
2020-02-24 21:50:30 -0600

    second-commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import ListaInmuebles from "./components/vistas/ListaInmuebles";
 import AppNavbar from "./components/Layout/AppNavbar";
 import { BrowserRouter as Router,Switch, Route } from "react-router-dom";
-import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
 import theme from "./theme/theme";
 import RegistrarUsuarios from "./components/security/RegistrarUsuarios";
 
@@ -18,15 +18,13 @@ class App extends Component {
 
           <Grid container>
             <Switch>
-              <Route path="/" exact components={ListaInmuebles}></Route>
-              <Route path="/auth/registrarUsuario" exact components={RegistrarUsuarios}></Route>
+              <Route exact path="/" component={ListaInmuebles}></Route>
+              <Route exact path="/auth/registrarUsuario" component={RegistrarUsuarios}></Route>
             </Switch>
           </Grid>
-          <ListaInmuebles />
         </MuiThemeProvider>
       </Router>
     );
-  }
-}
+  }}
 
 export default App;
--- src/components/security/RegistrarUsuarios.js
+++ src/components/security/RegistrarUsuarios.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { Container, Avatar, Typography, Grid, TextField } from "@material-ui/core";
+import {
+  Container,
+  Avatar,
+  Typography,
+  Grid,
+  TextField,
+  Button
+} from "@material-ui/core";
 import LockOutLineIcon from "@material-ui/icons/LockOutlined";
 
 const style = {
@@ -14,12 +21,38 @@ const style = {
     backgraoundColor: "#e53935"
   },
   form: {
-      width: "100%",
-      marginTop: 10
+    width: "100%",
+    marginTop: 10
+  },
+  submit: {
+      marginTop: 15,
+      marginBottom: 20
   }
 };
 
 class RegistrarUsuarios extends Component {
+    state= {
+        usuario:{
+            nombre:'',
+            apellidos:'',
+            email:'',
+            password:''
+        }
+    }
+
+    onChange = e => {
+        let usuario = Object.assign({}, this.state.usuario);
+        usuario[e.target.name] = e.target.value;
+        this.setState ({
+            usuario: usuario
+        })
+    }
+
+    RegistrarUsuarios = e => {
+        e.preventDefault();
+        console.log('imprimir usuario', this.state.usuario);
+    }
+    
   render() {
     return (
       <Container maxWidth="md">
@@ -28,14 +61,31 @@ class RegistrarUsuarios extends Component {
             <LockOutLineIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-              Registrate
+            Registrate
           </Typography>
           <form styles={style.form}>
-              <Grid container spacing={2}>
-                  <Grid item md={6} xs={12}>
-                      <TextField name="nombre" fullWidth label="Ingrese su nombre" />
-                  </Grid>
+            <Grid container spacing={2}>
+              <Grid item md={6} xs={12}>
+                <TextField name="nombre" onChange={this.onChange} value={this.state.usuario.nombre} fullWidth label="Ingrese su nombre" />
               </Grid>
+              <Grid item md={6} xs={12}>
+                <TextField name="apellidos" onChange={this.onChange} value={this.state.usuario.apellido} fullWidth label="Ingrese sus apellidos"
+                />
+              </Grid>
+              <Grid item md={6} xs={12}>
+                <TextField name="email" onChange={this.onChange} value={this.state.usuario.email} fullWidth label="Email" />
+              </Grid>
+              <Grid item md={6} xs={12}>
+                <TextField type="password" onChange={this.onChange} value={this.state.usuario.password} name="password" fullWidth label="Password" />
+              </Grid>
+            </Grid>
+            <Grid container justify="center">
+                <Grid item xs={12} md={6}>
+                    <Button type="submit" onClick={this.RegistrarUsuarios} variant="contained" fullWidth size="large" color="primary" style={style.submit}>
+                        Registrar
+                    </Button>
+                </Grid>
+            </Grid>
           </form>
         </div>
       </Container>
